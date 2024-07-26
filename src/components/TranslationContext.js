23afe9534a2d41a17645dea75b30e1fa5ac71f8f
Frozen Packet <chuck.pitre@hotmail.com>
2024-07-25 23:32:08 -0400

    border=0

--- src/components/TranslationContext.js
+++ src/components/TranslationContext.js
@@ -23,7 +23,7 @@ export const TranslationProvider = ({ children }) => {
       <p>We are delighted to host this gathering so everyone can meet each other. Our family reunion will take place on August 13, 2024, at <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">La Vieille École</a> from noon to 3 PM.</p>
       <p>We invite you to share your thoughts and messages in our guest book to make this event even more memorable.</p>
       <p>La Vieille École restaurant offers us the following options:</p>
-      <table border=1>
+      <table border=0>
         <tr>
           <td style="padding: 30px;"></td>
           <td style="text-align: left;">Lobster Roll with Chips</td>
@@ -86,7 +86,7 @@ export const TranslationProvider = ({ children }) => {
       <p>Nous sommes ravis d'organiser cette rencontre pour que tout le monde puisse se rencontrer. Notre réunion de famille aura lieu le 13 août 2024 à <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">La Vieille École</a> de midi à 15h.</p>
       <p>Nous vous prêterons une plume pour nous écrire des mots, partager vos souvenirs et laisser un message dans notre livre d'or.</p>
       <p>Le restaurant La Vieille École nous offre les options suivantes :</p>
-      <table border=1>
+      <table border=0>
         <tr>
           <td style="padding: 30px;"></td>
           <td style="text-align: left;">Lobster Roll avec Chips</td>
