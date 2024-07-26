a7ee339e586925f24f4b0aa3397b3f97a4656f78
Frozen Packet <chuck.pitre@hotmail.com>
2024-07-25 21:52:16 -0400

    aboutr.js modified as well as TranslationContext.js

--- src/components/aboutreunion/aboutr.js
+++ src/components/aboutreunion/aboutr.js
@@ -1,26 +1,19 @@
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
 import Countdown from '../countdown/countdown';
 import RSVP from '../rsvp/RSVP';
 import './aboutr.css';
-// import { TranslationContext } from '../TranslationContext';
-// import React, { useContext } from 'react';
 
 function AboutReunion() {
-  // We can use the context if needed, but for now, we'll just use the FormattedMessage component
-  // const { locale, setLocale, messages } = useContext(TranslationContext);
-
   return (
     <div className="about-reunion">
       <h1><FormattedMessage id="welcome" /></h1>
       <p><FormattedMessage id="celebrate" /></p>
-      <p>
-        <FormattedMessage 
-          id="wherearewe"
-          values={{
-            link: (...chunks) => <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">{chunks}</a>
-          }}
-        />
-      </p>
+      <FormattedHTMLMessage
+        id="wherearewe"
+        values={{
+          link: (...chunks) => <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">{chunks}</a>
+        }}
+      />
       <p><FormattedMessage id="rsvpInfo" /></p>
       <RSVP />
       <Countdown />
