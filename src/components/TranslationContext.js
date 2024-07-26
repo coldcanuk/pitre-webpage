0d7821d204f18d0c9da6600813eba6d0ab211e72
Frozen Packet <chuck.pitre@hotmail.com>
2024-07-26 00:28:58 -0400

    tweaking

--- src/components/TranslationContext.js
+++ src/components/TranslationContext.js
@@ -10,6 +10,7 @@ export const TranslationProvider = ({ children }) => {
   const enMessages = {
     home: "Home",
     about: "About the Reunion",
+    schedule: "Schedule",
     gallery: "Gallery",
     ref: "References",
     welcome: "Welcome to the Pitre Family Reunion 2024",
@@ -19,53 +20,60 @@ export const TranslationProvider = ({ children }) => {
     xFeed: "We have also created an X feed displayed below. Please give us a follow!",
     cma: "Check out the <link>CMA2024 webpage</link> for all the fun activities and details about the <link2>family reunions</link2>.",
     wherearewe: `
-      <p>We are delighted to host this gathering so everyone can meet each other. Our family reunion will take place on August 13, 2024, at <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">La Vieille École</a> from <b>noon to 3 PM.</b></p>
+      <p>We are delighted to host this gathering so everyone can meet each other. Our family reunion will take place on August 13, 2024, at <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">La Vieille École</a> from noon to 3 PM.</p>
       <p>We invite you to share your thoughts and messages in our guest book to make this event even more memorable.</p>
       <p>La Vieille École restaurant offers us the following options:</p>
-      <table border=0>
+      <table border=1>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Lobster Roll with Chips</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">17 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Snow Crab Roll with Chips</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">17 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Seafood Chowder</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">18 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Spaghetti with Homemade Sauce</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">18 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Haddock Dinner with vegetables and mash</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">21 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Hot Lobster on Pastry Shell with vegetables and mash</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">25 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Whole Lobster with Potato Salad</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">30 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Snow Crab Clusters with Potato Salad, Butter, and Dinner Roll</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">30 $</td>
         </tr>
       </table>
       <p>We look forward to meeting everyone!</p>
-      <p>Directions via <A HREF="https://tinyurl.com/yd5pw8x9" target="_blank" rel="noopener noreferrer">Google Maps</A></p>
     `
   };
 
@@ -73,62 +81,70 @@ export const TranslationProvider = ({ children }) => {
   const frMessages = {
     home: "Page d'accueil",
     about: "Réunion Pitre 2024",
+    schedule: "Horaire",
     gallery: "Galerie",
     ref: "Référence",
     welcome: "Bienvenue à la réunion de famille Pitre 2024",
     celebrate: "Rejoignez-nous pour célébrer notre héritage acadien et créer des souvenirs qui dureront toute une vie.",
-    rsvpInfo: "Préparons notre réunion de famille ! Veuillez remplir rapidement le formulaire de réponse avec votre adresse, e-mail ou numéro de téléphone cellulaire. Nous vous enverrons une invitation formelle et vous tiendrons informés par des messages électroniques à mesure que le grand jour approche.",
+    rsvpInfo: "Préparons notre réunion de famille ! Veuillez remplir rapidement le formulaire de réponse avec votre adresse, e-mail ou numéro de téléphone portable. Nous vous enverrons une invitation formelle et vous tiendrons informés par des messages électroniques à mesure que le grand jour approche.",
     galleryInfo: "La section Galerie est maintenant en ligne! Nous avons un visualiseur qui charge actuellement 1 image et 1 vidéo. Nous le peuplerons dans les jours à venir. Les images sont générées à l'aide de Midjourney et Leonardo.AI. Les images ont ensuite été chargées dans RunwayML pour créer des vidéos à partir des images. J'ai ensuite utilisé ClipChamp pour les assembler.",
     xFeed: "Nous avons également créé un flux X affiché ci-dessous. Veuillez nous suivre!",
     cma: "Consultez la <link>page web CMA2024</link> pour découvrir toutes les activités amusantes et les informations sur les <link2>réunions de famille</link2>.",
     wherearewe: `
-      <p>Nous sommes ravis d'organiser cette rencontre pour que tout le monde puisse se rencontrer. Notre réunion de famille aura lieu le 13 août 2024 à <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">La Vieille École</a> de <b>midi à 15h</b>.</p>
+      <p>Nous sommes ravis d'organiser cette rencontre pour que tout le monde puisse se rencontrer. Notre réunion de famille aura lieu le 13 août 2024 à <a href="https://www.ecoledanslanse.com/" target="_blank" rel="noopener noreferrer">La Vieille École</a> de midi à 15h.</p>
       <p>Nous vous prêterons une plume pour nous écrire des mots, partager vos souvenirs et laisser un message dans notre livre d'or.</p>
       <p>Le restaurant La Vieille École nous offre les options suivantes :</p>
-      <table border=0>
+      <table border=1>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Lobster Roll avec Chips</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">17 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Snow Crab Roll avec Chips</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">17 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Seafood Chowder</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">18 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Spaghetti avec Sauce Maison</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">18 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Dîner de Haddock avec légumes et purée</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">21 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Homard chaud sur coquille de pâtisserie avec légumes et purée</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">25 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
+          <td style="padding: 20px;"></td>
           <td style="text-align: left;">Homard entier avec salade de pommes de terre</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">30 $</td>
         </tr>
         <tr>
-          <td style="padding: 40px;"></td>
-          <td style="text-align: left;">Clusters de crabe des neiges avec salade de pommes de terre, beurre et petit pain   </td>
+          <td style="padding: 20px;"></td>
+          <td style="text-align: left;">Clusters de crabe des neiges avec salade de pommes de terre, beurre et petit pain</td>
+          <td style="padding: 4px;"></td>
           <td style="text-align: right;">30 $</td>
         </tr>
       </table>
       <p>On a hâte de rencontrer tout le monde !</p>
-      <p>Directions via <A HREF="https://tinyurl.com/yd5pw8x9" target="_blank" rel="noopener noreferrer">Google Maps</A></p>
     `
   };
 
